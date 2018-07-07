b484e8b62af145ed472043cb58171f64175ce41c
hassanngala099 <hassan@lorisystems.com>
2018-07-07 17:49:50 +0300

    Create a home route

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return "hi you are on a dead link"
+
 if __name__ == "__main__":
     app.run()
