744edbff60985be40b031762def1635452804b96
hassanngala099 <hassan@lorisystems.com>
2018-07-10 18:52:44 +0300

    Add db.html

--- app.py
+++ app.py
@@ -1,38 +1,46 @@
-from flask import Flask,render_template
-
-#import MySQL
-
-app = Flask(__name__)
-#import this to enable database connection
+from flask import Flask
+from flask import render_template
 from flaskext.mysql import MySQL
+#from flask_mysqldb import MySQL
+mysql = MySQL()
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'lori'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
-# MySQL configurations
-
-
-
-def conect():
-	if conn:
-		return "Connected to DB"
-	else:
-		return "DB Refused connection"
-
+#creating a request handler
 @app.route("/")
-def home():
-	return "hi you are on a dead link"
+def index():
+	return "Hi There Welcome <b>to  Client Lori End Products"
 
-@app.route("/today")
-def returnrecords():
-	mysql = MySQL()
-	data = cursor.fetchall()
-	app.config['MYSQL_DATABASE_USER'] = 'root'
-	app.config['MYSQL_DATABASE_PASSWORD'] = ''
-	app.config['MYSQL_DATABASE_DB'] = 'lori'
-	app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-	mysql.init_app(app)
-	conn = mysql.connect()
-	cursor = conn.cursor()
-	data = cursor.fetchall()
-	return render_template("yes.html",data=data)
-	if __name__ == '__main__':
-		app.run()
+
+@app.route("/db",methods=['GET','POST'])
+def data():
+	#CREATE CURSOR
+	#conn=mysql.connect
+	#conn = mysql.connect()
+
+	#cur=mysql.connect.cursor()
+	#cur = conn.cursor()
+	cursor=mysql.connect().cursor()
+
+
+
+	#cur.execute("SELECT * FROM Records")
+	query1= "SELECT * FROM Records"
+	cursor.execute(query1)
+
+	#commit ro DB
+	data2=cursor.fetchall()
+	return render_template("db.html",data=data2)
+
+
+
+
+
+
+if __name__ == '__main__':
+	app.run(host='localhost', debug=True)
 		
