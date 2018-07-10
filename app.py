ab88e78e49ccf6173f856836889bdf674959413b
hassanngala099 <hassan@lorisystems.com>
2018-07-10 11:57:35 +0300

    Add new functions to app.py

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from flask import Flask,render_template
+
+#import MySQL
+
 app = Flask(__name__)
+#import this to enable database connection
+from flaskext.mysql import MySQL
+
+# MySQL configurations
+
+
+
+def conect():
+	if conn:
+		return "Connected to DB"
+	else:
+		return "DB Refused connection"
 
 @app.route("/")
 def home():
@@ -7,7 +22,17 @@ def home():
 
 @app.route("/today")
 def returnrecords():
-	return render_template(today.html)
-
-if __name__ == "__main__":
-    app.run()
+	mysql = MySQL()
+	data = cursor.fetchall()
+	app.config['MYSQL_DATABASE_USER'] = 'root'
+	app.config['MYSQL_DATABASE_PASSWORD'] = ''
+	app.config['MYSQL_DATABASE_DB'] = 'lori'
+	app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+	mysql.init_app(app)
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	data = cursor.fetchall()
+	return render_template("yes.html",data=data)
+	if __name__ == '__main__':
+		app.run()
+		
